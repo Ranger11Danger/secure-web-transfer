54d99ca653b072ec7042c1825fc4a496d9f1679f
ranger11danger <ranger11danger@necromancerlabs.com>
2023-09-28 02:17:05 -0400

    upadate docker hub image name in run

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -p 443:443 -v $(pwd):/app/serve secure_serve
+docker run --rm -it -p 443:443 -v $(pwd):/app/serve ranger11danger/secure_serve || docker run --rm -it -p 443:443 -v $(pwd):/app/serve secure_serve
